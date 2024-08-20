b3f66db66dd802f62d07552229040275165ec76b
bhanu <kumarbhanu25@gmail.com>
2024-08-20 16:33:49 +0530

    intial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.querySelector(".navbar__mobile-menu-toggle");
+    const mobileMenu = document.querySelector(".navabar__mobile-menu-items");
+    toggleButton.addEventListener("click", function () {
+      mobileMenu.classList.toggle("active");
+    });
+  });
+  
+//   change navabar background on scroll
+  window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    console.log(navbar);
+    if (window.scrollY > 0) {
+      navbar.classList.add("navbar-scroll");
+    } else {
+      navbar.classList.remove("navbar-scroll");
+    }
+  });
+  
